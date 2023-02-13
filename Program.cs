f224cd55dc651c4dd4d63a9108c8c9f710a2f08c
VladimirVysochanskiy <shunkar78@mail.ru>
2023-02-13 12:48:56 +0300

    Print1Darray and FillArraymanual created

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
 ﻿
+
+
+void Print1DArray(string[] array)
+{
+    Console.Write("| ");
+    Console.Write(string.Join(" | ", array));
+    Console.WriteLine(" |");
+}
+
+string[] fillArrayManual(string text)
+{
+    string[] array = {};
+    while(true)
+    {
+        Console.Write(text);
+        string? txt = (Console.ReadLine());
+        if (txt == "") break;
+        array = array.Append(txt !).ToArray();
+    }
+    return array;
+}
+
+string[] myArray = fillArrayManual("Введите любые символы или ВВОД для выхода:\n");
+Console.WriteLine();
+Print1DArray(myArray);
