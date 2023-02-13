5ee665f2937bad7dd2d586a67a6e8d633cac6dbc
VladimirVysochanskiy <shunkar78@mail.ru>
2023-02-13 15:11:21 +0300

    Method for creation and filling array added

--- Program.cs
+++ Program.cs
@@ -21,6 +21,23 @@ string[] FillArrayManual(string text)
     return array;
 }
 
-string[] myArray = FillArrayManual("Введите любые символы или ВВОД для выхода:\n");
-Console.WriteLine();
-Print1DArray(myArray);
+string[] CreateAndFillRandomArray(int length, int strMax, int fromChar, int toChar)
+{
+    string[] array = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        string text = "";
+        int strLen = new Random().Next(1, strMax);
+        for (int j = 0; j < strLen; j++)
+        {
+            text += Convert.ToChar(new Random().Next(fromChar, toChar));
+        }
+        array[i] = text;
+    }
+    return array;
+}
+
+// string[] myArray = CreateAndFillRandomArray(10, 10, 1040, 1103);
+// string[] myArray = FillArrayManual("Введите любые символы или ВВОД для выхода:\n");
+// Console.WriteLine();
+// Print1DArray(myArray);
